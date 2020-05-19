3be8e2157070f8d582e326218c7f7b8a355230e9
Maciej Piechoczek <maciej.piechoczek@itcraftship.com>
2020-05-19 14:27:35 +0200

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Maciej Piechoczek on 19/05/2020.
+//  Copyright © 2020 Maciej Piechoczek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
