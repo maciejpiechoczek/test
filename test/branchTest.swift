ee6eccc24a605902099c3e479254d2677136bf63
Maciej Piechoczek <maciej.piechoczek@itcraftship.com>
2020-05-26 14:01:42 +0200

    Squash test

---
+++ test/branchTest.swift
@@ -0,0 +1,13 @@
+//
+//  branchTest.swift
+//  test
+//
+//  Created by Maciej Piechoczek on 26/05/2020.
+//  Copyright © 2020 Maciej Piechoczek. All rights reserved.
+//
+
+import Foundation
+
+class BranchTest {
+    var testVar: String?
+}
